134788098a3592d9aa6c59de660ed19443de3de7
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-20 15:57:02 +0900

    add contact view

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,12 +1,167 @@
-import { Heading, Box } from "@chakra-ui/react";
+import {
+  Heading,
+  Box,
+  Container,
+  Flex,
+  Wrap,
+  WrapItem,
+  Text,
+  VStack,
+  Button,
+  HStack,
+  IconButton,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from "@chakra-ui/react";
+import { MdEmail, MdLocationOn, MdOutlineEmail } from "react-icons/md";
+import { BsGithub, BsDiscord, BsPerson, BsTwitter } from "react-icons/bs";
 import Bread from "../bread/index";
 
 const Page = () => {
   return (
-    <Box bg="white" px="15px" py="5px">
+    <Box bg="clear" px="15px" py="5px">
       <Bread title="Contact" />
-
-      <Heading color="orange.400">Contact</Heading>
+      <Container bg="clear" maxW="full" mt={0} centerContent overflow="hidden">
+        <Flex>
+          <Box
+            bg="white"
+            color="blackAlpha.800"
+            borderRadius="lg"
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ sm: 5, md: 5, lg: 16 }}
+            boxShadow="base"
+            rounded="md"
+          >
+            <Box p={4}>
+              <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+                <WrapItem>
+                  <Box>
+                    <Heading>Contact</Heading>
+                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
+                      Fill up the form below to contact
+                    </Text>
+                    <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                      <VStack pl={0} spacing={3} alignItems="flex-start">
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="300px"
+                          variant="ghost"
+                          color="blackAlpha.700"
+                          _hover={{ border: "2px solid #1C6FEB" }}
+                          leftIcon={<MdEmail color="#1970F1" size="20px" />}
+                        >
+                          hogehoge.fugafuga@example.com
+                        </Button>
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="110px"
+                          variant="ghost"
+                          color="blackAlpha.700"
+                          _hover={{ border: "2px solid #1C6FEB" }}
+                          leftIcon={
+                            <MdLocationOn color="#1970F1" size="20px" />
+                          }
+                        >
+                          Japan
+                        </Button>
+                      </VStack>
+                    </Box>
+                    <HStack
+                      mt={{ lg: 10, md: 10 }}
+                      spacing={5}
+                      px={5}
+                      alignItems="flex-start"
+                    >
+                      <IconButton
+                        aria-label="twitter"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: "#0D74FF" }}
+                        color="blackAlpha.700"
+                        icon={<BsTwitter size="28px" />}
+                      />
+                      <IconButton
+                        aria-label="github"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: "#0D74FF" }}
+                        color="blackAlpha.700"
+                        icon={<BsGithub size="28px" />}
+                      />
+                      <IconButton
+                        aria-label="discord"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: "#0D74FF" }}
+                        color="blackAlpha.700"
+                        icon={<BsDiscord size="28px" />}
+                      />
+                    </HStack>
+                  </Box>
+                </WrapItem>
+                <WrapItem>
+                  <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                      <VStack spacing={5}>
+                        <FormControl id="name">
+                          <FormLabel>Your Name</FormLabel>
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<BsPerson color="gray.800" />}
+                            />
+                            <Input type="text" size="md" isRequired />
+                          </InputGroup>
+                        </FormControl>
+                        <FormControl id="name">
+                          <FormLabel>Mail</FormLabel>
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<MdOutlineEmail color="gray.800" />}
+                            />
+                            <Input type="text" size="md" isRequired />
+                          </InputGroup>
+                        </FormControl>
+                        <FormControl id="name">
+                          <FormLabel>Message</FormLabel>
+                          <Textarea
+                            borderColor="gray.300"
+                            _hover={{
+                              borderRadius: "gray.300",
+                            }}
+                            placeholder="message"
+                            isRequired
+                          />
+                        </FormControl>
+                        <FormControl id="name" float="right">
+                          <Button
+                            variant="solid"
+                            bg="#0D74FF"
+                            color="white"
+                            _hover={{}}
+                          >
+                            Send Message
+                          </Button>
+                        </FormControl>
+                      </VStack>
+                    </Box>
+                  </Box>
+                </WrapItem>
+              </Wrap>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
     </Box>
   );
 };
