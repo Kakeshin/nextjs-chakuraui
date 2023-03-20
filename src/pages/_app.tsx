984d1221fb3db453d0294a2a9f8724cb40546d9b
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-20 18:26:33 +0900

    add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import chakraTheme from "@chakra-ui/theme";
 import type { AppProps } from "next/app";
 import Header from "./header";
+import Head from "next/head";
 
 const { Heading, Form, FormLabel, Input, Checkbox, Link, Button } =
   chakraTheme.components;
@@ -27,10 +28,23 @@ const theme = extendTheme({
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keyword"
+          content="かけしん, カケシン, kakeshin, Kakeshin, dev, Dev, kakeshin dev, kekeshin Dev, kakeshinDev, Kakeshin Dev, Kakeshin dev, KakeshinDev"
+        />
+        <meta
+          name="description"
+          content="Webサイト開発の自己学習を行なっている道産子エンジニア"
+        />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
