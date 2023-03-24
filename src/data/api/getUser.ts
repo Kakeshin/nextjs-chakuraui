82a38775e83f091df657d5799743e41188c77615
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-24 16:21:42 +0900

    fix

--- src/data/api/getUser.ts
+++ src/data/api/getUser.ts
@@ -5,14 +5,21 @@ import { UserRequest } from '@/data/types/request/getUserRequest';
 const getUser = async (userRequest: UserRequest): Promise<UserResponse> => {
   const baseUrl = process.env.BASE_URL;
   const url = `${baseUrl}/mailer`;
-  const result = await axios.post(url, {
-    token: process.env.MAILR_TOKEN,
-    name: userRequest.name,
-    address: userRequest.email,
-    message: userRequest.message,
-  });
 
-  return result.data;
+  try {
+    const result = await axios.post(url, {
+      token: process.env.MAILER_TOKEN,
+      name: userRequest.name,
+      address: userRequest.email,
+      message: userRequest.message,
+    });
+
+    return result.data;
+  } catch (error) {
+    return {
+      name: `${error}`,
+    };
+  }
 };
 
 export default getUser;
