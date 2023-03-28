c3c73b65c5bb99a2ee5bfe218292663d4708bb16
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-28 18:36:41 +0900

    fix

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -4,27 +4,21 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Page() {
-  const [fetched, setFetched] = useState(false);
   const [name, setName] = useState('');
   const [url, setUrl] = useState('');
   const [twitter, setTwitter] = useState('');
 
   useEffect(() => {
-    if (fetched) return;
-    console.log('Axe');
     const api = async () => {
       const { data } = await await axios.get('./api/github');
       return data;
     };
-    api()
-      .then((res) => {
-        setUrl(res.url);
-        setName(res.name);
-        setTwitter(res.twitter);
-        setFetched(true);
-      })
-      .catch((e) => console.error(e));
-  }, [fetched]);
+    api().then((res) => {
+      setUrl(res.url);
+      setName(res.name);
+      setTwitter(res.twitter);
+    });
+  }, []);
 
   return (
     <Box>
