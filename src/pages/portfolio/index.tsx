e15ad37b07289ec8edbb3183e68fd51aa1d68967
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-28 18:25:17 +0900

    fix

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,20 +1,30 @@
 import { Box, Heading } from '@chakra-ui/react';
 import Bread from '@/pages/bread/index';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Page() {
+  const [fetched, setFetched] = useState(false);
   const [name, setName] = useState('');
   const [url, setUrl] = useState('');
   const [twitter, setTwitter] = useState('');
-  const handler = async () => {
-    const res = await (await axios.get('./api/github')).data;
-    setUrl(res.url);
-    setName(res.name);
-    setTwitter(res.twitter);
-  };
 
-  handler();
+  useEffect(() => {
+    if (fetched) return;
+    console.log('Axe');
+    const api = async () => {
+      const { data } = await await axios.get('./api/github');
+      return data;
+    };
+    api()
+      .then((res) => {
+        setUrl(res.url);
+        setName(res.name);
+        setTwitter(res.twitter);
+        setFetched(true);
+      })
+      .catch((e) => console.error(e));
+  }, [fetched]);
 
   return (
     <Box>
