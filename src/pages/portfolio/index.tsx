7c4e9f77c35ac0f92701609bc98ed75bda208251
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-29 17:45:18 +0900

    fix

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,31 +1,20 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Bread from '@/pages/bread/index';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 function Page() {
-  const [name, setName] = useState('');
-  const [url, setUrl] = useState('');
-  const [twitter, setTwitter] = useState('');
-
-  useEffect(() => {
-    const api = async () => {
-      const { data } = await await axios.get('./api/github');
-      return data;
-    };
-    api().then((res) => {
-      setUrl(res.url);
-      setName(res.name);
-      setTwitter(res.twitter);
-    });
-  }, []);
-
   return (
-    <Box>
+    <Box bg="white" px="15px" py="5px">
       <Bread title="Portfolio" />
-      <Heading>{name}</Heading>
-      <Heading>{url}</Heading>
-      <Heading>{twitter}</Heading>
+      <Box
+        w="full"
+        mt="10px"
+        p="10px"
+        border="1px"
+        borderColor="blackAlpha.400"
+        borderRadius="5px"
+        boxShadow="base"
+        rounded="md"
+      />
     </Box>
   );
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,25 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import Bread from '@/pages/bread/index';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Page() {
+  const [name, setName] = useState('');
+  const [url, setUrl] = useState('');
+  const [twitter, setTwitter] = useState('');
+
+  useEffect(() => {
+    const api = async () => {
+      const { data } = await await axios.get('./api/github');
+      return data;
+    };
+    api().then((res) => {
+      setUrl(res.url);
+      setName(res.name);
+      setTwitter(res.twitter);
+    });
+  }, []);
+
   return (
     <Box bg="white" px="15px" py="5px">
       <Bread title="Profile" />
@@ -15,7 +33,11 @@ function Page() {
         borderRadius="5px"
         boxShadow="base"
         rounded="md"
-      />
+      >
+        <Heading>{name}</Heading>
+        <Heading>{url}</Heading>
+        <Heading>{twitter}</Heading>
+      </Box>
     </Box>
   );
 }
